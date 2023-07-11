a3a8ec63b4332566e45bc471c85210e81cb8495c
SaraDaum <sara.daum.il@users.noreply.github.com>
2023-07-11 14:51:12 +0300

    Add code to Sensor class

--- src/WS1/Observables/Sensor.java
+++ src/WS1/Observables/Sensor.java
@@ -1,4 +1,18 @@
 package WS1.Observables;
 
 public class Sensor {
+    public int lastReading = 0;
+    public int interval = 0;
+   public String type = "";
+
+    public Sensor(String s, int i){
+       type = s;
+       interval = i;
+    }
+
+    protected int read(){
+        return 0;
+    }
+
+    public void check(){}
 }
