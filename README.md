c2a2b1c2d3c578bde0344ae6fe1b0daeafc69a8c
SaraDaum <sara.daum.il@users.noreply.github.com>
2023-07-11 13:19:22 +0300

    All Files

---
+++ README.md
@@ -0,0 +1,2 @@
+# homeWork9
+HomeWork in  "Object-oriented design and programming for engineering" course
---
+++ src/WS1/System/MainClassWS1.java
@@ -0,0 +1,9 @@
+package WS1.System;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class MainClassWS1 {
+    public static void main(String[] args) {
+
+        }
+    }
